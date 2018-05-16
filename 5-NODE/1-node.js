d09f862f049877dae34ae038540b52c1973fcda5
JAreina <jareinafdez@gmail.com>
2018-05-16 09:47:39 +0200

    a

--- 5-NODE/1-node.js
+++ 5-NODE/1-node.js
@@ -1,24 +1,13 @@
 const http = require('http');
 
+const servidor = http.createServer((req, res) => {
+	console.log(req.headers);
+	console.log('peticion recibida');
+	let datos = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+	let d = [...datos ];
 
+	res.writeHead(200, { 'content-type': 'text/html' });
+	res.end(`<h1> generado servidor: ${d}</h1>`);
+});
 
-
-const servidor = http.createServer(
-    (req,res)=>{
-       
-        console.log(req.headers)
-         console.log("peticion recibida");
-         
-          res.writeHead(200,{"content-type":"text/html"});
-          res.end("<h1> generado servidor</h1>")
-    });
-
-  
-
-    servidor.listen(3002);
-    
-
-
-
-
-
+servidor.listen(3002);
