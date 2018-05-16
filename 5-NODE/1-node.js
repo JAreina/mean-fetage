3a50a5fd6940067ef86cdf508fb7e6575e380f00
JAreina <jareinafdez@gmail.com>
2018-05-16 09:08:11 +0200

    node

--- 5-NODE/1-node.js
+++ 5-NODE/1-node.js
@@ -1,3 +1,24 @@
-const fs = require('fs');
+const http = require('http');
+
+
+
+
+const servidor = http.createServer(
+    (req,res)=>{
+       
+        console.log(req.headers)
+         console.log("peticion recibida");
+         
+          res.writeHead(200,{"content-type":"text/html"});
+          res.end("<h1> generado servidor</h1>")
+    });
+
+  
+
+    servidor.listen(3002);
+    
+
+
+
 
 
